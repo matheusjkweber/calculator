73275f261275e121b342abc203e8cb2e23160631
Matheus Weber <matheus.weber@ibm.com>
2019-06-10 09:46:46 -0300

    Add component button and start component display

---
+++ src/components/Display.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {
+    StyleSheet,
+    Text,
+    View
+} from 'react-native'
+
+const styles = StyleSheet.create({
+    display: {
+        flex: 1,
+        padding: 20,
+        justifyContent: 'center',
+        backgroundColor: 'rgba(0, 0, 0, 0.6',
+        alignItens: 'flex-end'
+    },
+    displayValue: {
+        fontSize: 60,
+        color: '#fff'
+    }
+})
+
+export default props =>
+    <View style={ styles.display }>
+        
+    </View>
