32c686ef28a3d0442492c16bc79757e729b6e07d
Matheus Weber <matheus.weber@ibm.com>
2019-06-10 13:25:07 -0300

    Add logic to calculator

--- App.js
+++ App.js
@@ -11,22 +11,66 @@ import {Platform, StyleSheet, Text, View} from 'react-native'
 import Button from './src/components/Button'
 import Display from './src/components/Display'
 
+const initalState = {
+  displayValue: '0',
+  clearDisplay: false,
+  operation: null,
+  values: [0, 0],
+  current: 0
+}
 export default class App extends Component {
   
-  state = {
-    displayValue: '0'
-  }
+  state = { ...initalState }
 
   addDigit = n => {
-    this.setState({ displayValue: n })
+    const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
+
+    if(n === '.' && !clearDisplay 
+      && this.state.displayValue.includes('.')) {
+      return
+    }
+
+    const currentValue = clearDisplay ? '' : this.state.displayValue
+    const displayValue = currentValue + n
+
+    this.setState({ displayValue, clearDisplay: false })
+
+    if(n !== '.') {
+      const newValue = parseFloat(displayValue)
+      const values = [...this.state.values]
+      values[this.state.current] = newValue
+      this.setState({ values })
+    }
   }
 
   clearMemory = () => {
-    this.setState({ displayValue: 0 })
+    this.setState({ ...initalState })
   }
 
   setOperation = operation => {
+    if(this.state.current === 0) {
+      this.setState({ operation, current: 1, clearDisplay: true })
+    } else {
+      const equals = operation === '='
+      const values = [...this.state.values]
+      if(values[1] !== 0){
+        try {
+          values[0] = eval(`${values[0]} ${this.state.operation} ${values[1]}`)
+        } catch(e) {
+          values[0] = this.state.values[0]
+        }
+      }
+      values[1] = 0
 
+      this.setState({
+        displayValue: `${values[0]}`,
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        //clearDisplay: !equals,
+        clearDisplay: true,
+        values
+      })
+    }
   }
 
   render() {
